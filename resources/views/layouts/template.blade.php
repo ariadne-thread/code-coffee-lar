ff226b9d2c9326b431e7f5046b982e2cefb62ac4
Camila Cestari <camilacestari@live.com>
2021-10-06 17:41:36 -0300

    add template blade

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,16 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    
-    <title>CodeCoffeeLar ☕️</title>
+    <title>@yield('title')</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
 <body>
-    <h1 class="text-center">CodeCoffeeLar</h1>
-    <h4 class="text-center">👩🏼‍💻 CRUD Laravel ☕️</h4>
-    <hr>
+    @yield('content')
 
     <!-- Optional JavaScript -->
 
