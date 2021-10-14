cc165c00e6c4b2622b11afe7d955fb357352d386
Camila Cestari <camilacestari@live.com>
2021-10-14 14:40:00 -0300

    Template Updates

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.template')
 @section('title', 'Products Page')
 @section('content')
-<h1 class="text-center">CodeCoffeeLar</h1>
-<h4 class="text-center">👩🏼‍💻 CRUD Laravel ☕️</h4>
-<hr>
-<h2 class="text-center">Products Insert Page</h2>
+    <h2 class="text-center">Products Insert Page</h2>
 @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,9 +2,6 @@
 @section('title', 'Products Page')
 @section('content')
 <div class="container">
-    <h1 class="text-center">CodeCoffeeLar</h1>
-    <h4 class="text-center">👩🏼‍💻 CRUD Laravel ☕️</h4>
-    <hr>
     <a href="products/insert" button type="button" class="mt-4 mb-4 btn btn-primary">Insert</button></a>
     <hr>
     @foreach($products as $product)
