9338f27ce5cc01b0cdafeeaf1cef58a3b81d481c
Camila Cestari <camilacestari@live.com>
2021-10-18 15:38:18 -0300

    Applying DataTables

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,43 @@
 @extends('layouts.template')
 @section('title', 'Products Page')
 @section('content')
+
 <div class="container">
-    <a href="products/insert" button type="button" class="mt-4 mb-4 btn btn-primary">Insert</button></a>
-    <hr>
-    @foreach($products as $product)
-    <p>{{$product->name}}</p>
-    @endforeach
-    {{$products->links()}}
+
+<a href="products/insert" button type="button" class="mt-4 mb-4 btn btn-primary">Insert Product</button></a>
+
+<!-- DataTales -->
+<div class="card shadow mb-4">
+
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Stock</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+    
+          <tbody>
+            @foreach($products as $product)
+            <tr>
+                <td>{{$product->name}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->value}}</td>
+                <td></td>
+            </tr>
+            @endforeach
+          </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
+
+    {{-- {{$products->links()}} --}}
+
 </div>
+
 @endsection
